3ba18a02973006004f5be5cd73a144fbc01c7b6b
gokulsp1995 <gokulsp2020@outlook.com>
2024-10-08 02:22:42 +0100

    Comments added further

--- banana.js
+++ banana.js
@@ -1,30 +1,30 @@
 // One banana solutions
 
-const numbers = ["One", "Two","Three","Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]; //Declaring the list of numbers
+const numbers = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]; //Declaring the list of numbers
 
 // Declaring an empty array to store the modified values
-let output=[];
+let output = [];
 // Entering a loop to iterate the array elements
-  for(let num in numbers) {
-    let result = numbers[num]; //Assigning each array elements to a new variable for performing operations  
-  
+for (let num in numbers) {
+  let result = numbers[num]; //Assigning each array elements to a new variable for performing operations  
+
   let includes_I = result.toLowerCase().includes("i"); //Checking for Capital and small letter "i"
   let includes_E = result.toLowerCase().includes("e"); //Checking for Capital and small letter "e"
 
-  if(includes_I && includes_E) { //for numbers having both i and e
+  if (includes_I && includes_E) { //for numbers having both i and e
     result += " yipes";        //string addition to include the required values
   }
-  else if(includes_I){
+  else if (includes_I) {
     result += " yip";
   }
-  else if(includes_E){
+  else if (includes_E) {
     result += " yep";
   }
 
-  output.push(result)
+  output.push(result);
 }
-console.log("Array",output);
+console.log("Array", output);  //The result in an array
 
 let string_list = output.join(" ");
-console.log("Answer in string:", string_list);
+console.log("Answer in string:", string_list); // result in string format
 
